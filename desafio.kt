df2252a8ad8e252d268ff802846620a328e80e90
Theadr29 <96799456+Theadr29@users.noreply.github.com>
2023-10-09 03:21:07 -0300

    Update desafio.kt
    Update do desafio Kotlin, feito por Adriano Almeida dos Santos, usuario Theadr29

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,83 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, var gral: Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
-    
+    var inscritos = mutableListOf<Usuario>()
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        // Verifica se o usuário já está inscrito
+        if (!inscritos.contains(usuario)) {
+            inscritos.add(usuario)
+            println("Seja bem vindo ${usuario.nome}, você foi matriculado no curso de $nome, veja a grade do curso:$conteudos")
+        } else {
+            println("Usuário ${usuario.nome} já está matriculado nesta formação")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+
+    val formacao =
+    Formacao(
+        nome="Analise e desenvolvimento de sistemas com Kotlin",
+        conteudos = listOf(
+
+            ConteudoEducacional(
+                nome = "Kotlin",
+                duracao =40,
+                gral = Nivel.DIFICIL
+
+            ),
+            ConteudoEducacional(
+                nome = "Jetpack Compose",
+                duracao = 30,
+                gral = Nivel.INTERMEDIARIO
+
+            ),
+            ConteudoEducacional(
+                nome = "Xml",
+                duracao = 20,
+                gral = Nivel.BASICO
+
+            ),
+
+            ConteudoEducacional(
+                nome = "Coroutines",
+                duracao = 15,
+                gral = Nivel.BASICO
+
+            ),
+            ConteudoEducacional(
+                nome = "Retrofit",
+                duracao = 15,
+                gral = Nivel.INTERMEDIARIO
+
+            ),
+            ConteudoEducacional(
+                nome = "Ktor",
+                duracao = 20,
+                gral = Nivel.INTERMEDIARIO
+
+            ),
+
+            ConteudoEducacional(
+                nome = "Spring",
+                duracao = 15,
+                gral = Nivel.INTERMEDIARIO
+
+            ),
+
+            )
+    )
+
+    val usuario = Usuario("Adriano Almeida dos Santos")
+    formacao.matricular(usuario = usuario)
+
+
 }
